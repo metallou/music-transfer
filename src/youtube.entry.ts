de8c2a93a707269f653b03a308a08ebd371f2f74
Kévin CASTEJON <kevincastejon13@gmail.com>
2023-12-05 20:52:34 +0100

    test

--- src/youtube.entry.ts
+++ src/youtube.entry.ts
@@ -1,5 +1,27 @@
 (
   (): void => {
-    console.debug('youtube');
+	let loop = true;
+	
+	let test = 10;
+	while (loop) {
+	  loop = false;
+	  test--;
+	  const buttonToLike = document.querySelector('.like [aria-pressed="false"]');
+	  if (buttonToLike instanceof HTMLButtonElement) {
+	    loop = true;
+		
+		button.scrollIntoView({
+            behavior: 'auto',
+            block: 'center',
+            inline: 'center',
+        });
+		
+		buttonToLike.click();
+	  }
+	  
+	  if (test === 0) {
+	    loop = false;
+	  }
+	}
   }
 )();
