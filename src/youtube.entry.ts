337dd47bfb8e2b3d55b03c8aa67d8d15de626946
Kévin CASTEJON <kevincastejon13@gmail.com>
2023-12-05 20:28:12 +0100

    test

---
+++ src/youtube.entry.ts
@@ -0,0 +1,5 @@
+(
+  (): void => {
+    console.debug('youtube');
+  }
+)();
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: {
+    "spotify": path.resolve(__dirname, 'src/spotify.entry.ts'),
+    "youtube": path.resolve(__dirname, 'src/youtube.entry.ts')
+  },
+  output: {
+    chunkFilename: '[name].js',
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+};
